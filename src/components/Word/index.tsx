e42e14b7cafc922f4299e8198b5bd5119ce75c42
Wiktor Musial <wiktormusial@icloud.com>
2022-03-21 20:21:20 +0100

    Fetch words in component

--- src/components/Word/index.tsx
+++ src/components/Word/index.tsx
@@ -1,28 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Switch } from "@mui/material";
+import { Words } from "./types";
 import WordHeader from "./WordHeader";
 import WordBody from "./WordBody";
 import WordFooter from "./WordFooter";
 import "./Word.css";
 
+const fetchWords = async () => {
+  const response = await axios.get<Words[]>(
+    "http://localhost:8000/api/v1/getwords/"
+  );
+  return response.data;
+};
+
 const Word = () => {
   const [translation, setTranslation] = useState(false);
+  const [words, setWords] = useState<Words[]>();
 
-  return (
-    <div className="card">
-      <div className="card__switch">
-        <div className="flag">🇵🇱</div>
-        <Switch
-          onChange={() => setTranslation(!translation)}
-          color={"primary"}
-        />
-        <div className="flag">🇺🇦</div>
+  useEffect(() => {
+    fetchWords()
+      .then((res) => setWords(res))
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!words) {
+    return <div>Loading</div>;
+  } else {
+    return (
+      <div className="card">
+        <div className="card__switch">
+          <div className="flag">🇵🇱</div>
+          <Switch
+            onChange={() => setTranslation(!translation)}
+            color={"primary"}
+          />
+          <div className="flag">🇺🇦</div>
+        </div>
+        <WordHeader>{!translation ? "się" : "себе"}</WordHeader>
+        <WordBody translation={translation} />
+        <WordFooter translation={translation} />
       </div>
-      <WordHeader>{!translation ? "się" : "себе"}</WordHeader>
-      <WordBody translation={translation} />
-      <WordFooter translation={translation} />
-    </div>
-  );
+    );
+  }
 };
 
 export default Word;
